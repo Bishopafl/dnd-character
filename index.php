ce0d4fd1b094fb30a15277741100b6b2b5dddf6f
Adam Lopez <Adamf04@gmail.com>
2018-12-03 23:09:22 -0500

    creating the page for my dungeons and dragons charatcer information

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<title>Dungeons and Dragons!</title>
+</head>
+<body>
+<!-- NAVIGATION BAR -->
+<?php include('resources/nav-bar.php'); ?>
+<!-- NAVIGATION BAR -->
+<div class="band">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="jumbotron jumbotron-fluid">
+				  <div class="container">
+				    <div class="row">
+				    	<div class="col-md-7">
+				    		<h1 class="display-4">Aasimar</h1>
+				    		<p class="lead">A dnd class that some backstory crap will go here blah</p>
+				    	</div>
+				    	<div class="col-md-">
+				    		<img src="https://via.placeholder.com/400x300">
+				    	</div>
+				    </div>
+				    
+				  </div>
+				</div>
+			</div>
+		</div>			
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<div style="background-color: cyan;">
+				<h3>Protector Aasimar</h3>
+				<p><strong>Ability Score Increase:</strong> Your Wisdom score increases by 1.</p>
+				<p><strong>Radiant Soul:</strong> Starting at 3rd level, you can use your action to unleash the divine energy within yourself, causing your eyes to glimmer and two luminous, incorporeal wings to sprout from your back.</p>
+				<p>Your transformation lasts for 1 minute or until you end it as a bonus action. During it, you have a flying speed of 30 feet, and once on each of your turns, you can deal extra radiant damage to one target when you deal damage to it with an attack or a spell. The extra radiant damage equals your level.</p>
+				<p>Once you use this trait, you can't use it again until you finish a long rest.</p>
+			</div>
+			
+		</div>
+		<div class="col-md-6">
+			<div style="background-color: yellow;">
+				<h3>Aasimar Traits</h3>
+				<ul>
+					<li><strong>Ability Score Increase:</strong> Your Charisma score increases by 2.</li>
+					<li><strong>Size:</strong> Medium.</li>
+					<li><strong>Speed:</strong> 30 feet.</li>
+					<li><strong>Darkvision:</strong> You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can't discern color in darkness, only shades of gray.</li>
+					<li><strong>Celestial Resistance:</strong> You have resistance to necrotic damage and radiant damage.</li>
+					<li><strong>Healing Hands:</strong> As an action, you can touch a creature and cause it to regain a number of hit points equal to your level. Once you use this trait, you can't use it again until you finish a long rest.</li>
+					<li><strong>Light Bearer:</strong> You know the Light cantrip. Charisma is your spellcasting ability for it.</li>
+					<li><strong>Languages:</strong> You can speak, read, and write Common and Celestial.</li>
+				</ul>
+			</div>
+			
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div style="background-color: red;">
+				<h2>Spells</h2>
+			</div>	
+		</div>
+		<div class="col-md-12">
+			<div class="col-md-12">
+			<div class="text-center">
+				<h3>Can use anytime</h3>
+				<span>Doesn't take up a spell slot</span>
+				<hr>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<h4>Disguise Self</h4>
+					<p>1st-level illusion</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 action </li>
+						<li>Range: Self </li>
+						<li>Components: V</li>
+						<li>Spell  Duration: 1 hour</li>
+					</ul>
+					<p>You make yourself – <strong>including your clothing, armor, weapons, and other belongings on your person </strong> – look different until the spell ends or until you use your action to dismiss it. You can seem 1 foot shorter or taller and can appear thin, fat, or in between...</p>
+					<!-- BUTTON TRIGGER MODAL -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#disguiseSelf">
+					  About
+					</button>
+					<!-- END BUTTON TRIGGER MODAL -->
+
+					<!-- MODAL -->
+					<div class="modal fade" id="disguiseSelf" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" id="exampleModalLabel">Disguise Self Information</h5>
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					      </div>
+					      <div class="modal-body">
+					        <p>You make yourself – <strong>including your clothing, armor, weapons, and other belongings on your person </strong> – look different until the spell ends or until you use your action to dismiss it. You can seem 1 foot shorter or taller and can appear thin, fat, or in between. <i>You can’t change your body type, so you must adopt a form that has the same basic arrangement of limbs.</i> Otherwise, the extent of the illusion is up to you.</p>
+				    		<p>The changes wrought by this spell fail to hold up to physical inspection. For example, if you use this spell to add a hat to your outfit, objects pass through the hat, and anyone who touches it would feel nothing or would feel your head and hair. If you use this spell to appear thinner than you are, the hand of someone who reaches out to touch you would bump into you while it was seemingly still in midair. To discern that you are disguised, a creature can use its action to inspect your appearance and must succeed on an Intelligence (Investigation) check against your spell save DC.</p>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					<!-- END MODAL -->
+				</div>
+				<div class="col-md-4">
+
+
+					<h4>Rope Trick</h4>
+					<p>2nd-level transmutation</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 action</li>
+						<li>Range: Self </li>
+						<li>Components: V</li>
+						<li>Spell  Duration: 1 hour</li>
+					</ul>
+					<p>You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends. The extradimensional space can be reached by climbing to the top of the rope. The space can hold as many as eight Medium or smaller creatures.</p>
+					<!-- BUTTON TRIGGER MODAL -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ropeTrick">
+					  About
+					</button>
+					<!-- END BUTTON TRIGGER MODAL -->
+
+					<!-- MODAL -->
+					<div class="modal fade" id="ropeTrick" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" id="exampleModalLabel">Disguise Self Information</h5>
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					      </div>
+					      <div class="modal-body">
+					        <p>You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends. The extradimensional space can be reached by climbing to the top of the rope. The space can hold as many as eight Medium or smaller creatures. The rope can be pulled into the space, making the rope disappear from view outside the space.</p>	
+							<p>Attacks and spells can’t cross through the entrance into or out of the extradimensional space, but those inside can see out of it as if through a 3-foot-by-5-foot window centered on the rope. Anything inside the extradimensional space drops out when the spell ends.</p>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					<!-- END MODAL -->
+				</div>
+			</div>
+		</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div style="background-color: red;">
+				<h2>Gloom Stalker Ranger Information</h2>
+			</div>	
+		</div>
+		<div class="col-md-12">
+			<div class="text-center">
+				<h3>Can use anytime</h3>
+				<span>Doesn't take up a spell slot</span>
+				<hr>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<h4>Disguise Self</h4>
+					<p>1st-level illusion</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 action </li>
+						<li>Range: Self </li>
+						<li>Components: V</li>
+						<li>Spell  Duration: 1 hour</li>
+					</ul>
+					<p>You make yourself – <strong>including your clothing, armor, weapons, and other belongings on your person </strong> – look different until the spell ends or until you use your action to dismiss it. You can seem 1 foot shorter or taller and can appear thin, fat, or in between. <i>You can’t change your body type, so you must adopt a form that has the same basic arrangement of limbs.</i> Otherwise, the extent of the illusion is up to you.</p>
+				    <p>The changes wrought by this spell fail to hold up to physical inspection. For example, if you use this spell to add a hat to your outfit, objects pass through the hat, and anyone who touches it would feel nothing or would feel your head and hair. If you use this spell to appear thinner than you are, the hand of someone who reaches out to touch you would bump into you while it was seemingly still in midair. To discern that you are disguised, a creature can use its action to inspect your appearance and must succeed on an Intelligence (Investigation) check against your spell save DC.</p>
+				</div>
+				<div class="col-md-4">
+
+
+					<h4>Rope Trick</h4>
+					<p>2nd-level transmutation</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 action</li>
+						<li>Range: Self </li>
+						<li>Components: V</li>
+						<li>Spell  Duration: 1 hour</li>
+					</ul>
+					<p>You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends. The extradimensional space can be reached by climbing to the top of the rope. The space can hold as many as eight Medium or smaller creatures. The rope can be pulled into the space, making the rope disappear from view outside the space.</p>	
+					<p>Attacks and spells can’t cross through the entrance into or out of the extradimensional space, but those inside can see out of it as if through a 3-foot-by-5-foot window centered on the rope. Anything inside the extradimensional space drops out when the spell ends.</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+	
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
