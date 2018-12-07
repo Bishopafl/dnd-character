d0cf2eb3a83e7b192fb870d18772f3d8fd35dd61
Adam Lopez <Adamf04@gmail.com>
2018-12-06 23:42:15 -0500

    updating some of the spell slots and created the js file

---
+++ resources/spells.php
@@ -0,0 +1,201 @@
+<div class="row">
+		<!-- SUB-HEADING -->
+		<div class="col-md-12">
+			<div style="background-color: red;">
+				<h2>Spells</h2>
+			</div>	
+		</div>
+		<!-- SUB-HEADING -->
+		<!-- MAIN CONTENT -->
+		<div class="col-md-12">
+			<div class="text-center">
+				<h3>Can use anytime</h3>
+				<span>Doesn't take up a spell slot</span>
+				<hr>
+			</div>
+			<div class="row">
+				<!-- ------------------------------------------------- -->
+				<!-- SPELL NUMBER ONE -->
+				<!-- ------------------------------------------------- -->
+				<div class="col-md-4">
+					<h4>Disguise Self</h4>
+					<p>1st-level illusion</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 action </li>
+						<li>Range: Self </li>
+						<li>Components: V</li>
+						<li>Spell  Duration: 1 hour</li>
+					</ul>
+					<p>You make yourself – <strong>including your clothing, armor, weapons, and other belongings on your person </strong> – look different until the spell ends or until you use your action to dismiss it. You can seem 1 foot shorter or taller and can appear thin, fat, or in between...</p>
+					<!-- BUTTON TRIGGER MODAL -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#disguiseSelf">
+					  About
+					</button>
+					<!-- END BUTTON TRIGGER MODAL -->
+
+					<!-- MODAL -->
+					<div class="modal fade" id="disguiseSelf" tabindex="-1" role="dialog" aria-labelledby="disguiseSelf" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" id="disguiseSelf">Disguise Self Information</h5>
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					      </div>
+					      <div class="modal-body">
+					        <p>You make yourself – <strong>including your clothing, armor, weapons, and other belongings on your person </strong> – look different until the spell ends or until you use your action to dismiss it. You can seem 1 foot shorter or taller and can appear thin, fat, or in between. <i>You can’t change your body type, so you must adopt a form that has the same basic arrangement of limbs.</i> Otherwise, the extent of the illusion is up to you.</p>
+				    		<p>The changes wrought by this spell fail to hold up to physical inspection. For example, if you use this spell to add a hat to your outfit, objects pass through the hat, and anyone who touches it would feel nothing or would feel your head and hair. If you use this spell to appear thinner than you are, the hand of someone who reaches out to touch you would bump into you while it was seemingly still in midair. To discern that you are disguised, a creature can use its action to inspect your appearance and must succeed on an Intelligence (Investigation) check against your spell save DC.</p>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					<!-- END MODAL -->
+				</div>
+				<!-- ------------------------------------------------- -->
+				<!-- SPELL NUMBER TWO -->
+				<!-- ------------------------------------------------- -->
+				
+			<!--
+
+				PUT THIS INFORMATION IN NEXT
+
+				Ensnaring Strike
+				1st-level conjuration
+				Casting Time: 1 bonus action
+				Range: Self
+				Components: V
+				Duration: Conc. Up to 1 minute
+
+				The next time you hit a creature with a weapon attack, the target must succeed on a Strength save or be restrained until the spell ends. A Large or bigger creature has advantage on this save.
+				While restrained by this spell, the target takes 1d6 piercing damage at the start of each of its turns. The creature restrained or one that can touch the creature can use its action to make a Strength check against your spell save DC. On a success, the target is freed.
+				At Higher Levels: The damage increases by 1d6 for each slot level above 1st. 
+			-->
+				<div class="col-md-4">
+					<h4>Ensnaring Strike</h4>
+					<p>2nd-level transmutation</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 action</li>
+						<li>Range: Self </li>
+						<li>Components: V</li>
+						<li>Spell  Duration: 1 hour</li>
+					</ul>
+					<p>You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends. </p>
+					<!-- BUTTON TRIGGER MODAL -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ensaringStrike">
+					  About
+					</button>
+					<!-- END BUTTON TRIGGER MODAL -->
+
+					<!-- MODAL -->
+					<div class="modal fade" id="ensaringStrike" tabindex="-1" role="dialog" aria-labelledby="ensaringStrike" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" id="ensaringStrike">Ensnaring Strike Information</h5>
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					      </div>
+					      <div class="modal-body">
+					        <p>The extradimensional space can be reached by climbing to the top of the rope. The space can hold as many as eight Medium or smaller creatures. You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends. The extradimensional space can be reached by climbing to the top of the rope. The space can hold as many as eight Medium or smaller creatures. The rope can be pulled into the space, making the rope disappear from view outside the space.</p>	
+							<p>Attacks and spells can’t cross through the entrance into or out of the extradimensional space, but those inside can see out of it as if through a 3-foot-by-5-foot window centered on the rope. Anything inside the extradimensional space drops out when the spell ends.</p>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					<!-- END MODAL -->
+				</div>
+				<!-- ------------------------------------------------- -->
+				<!-- SPELL NUMBER THREE -->
+				<!-- ------------------------------------------------- -->
+				<div class="col-md-4">
+					<h4>Hunter's Mark</h4>
+					<p>1st-level divination</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 bonus action</li>
+						<li>Range: 90 feet</li>
+						<li>Components: V</li>
+						<li>Duration: Conc. Up to 1 hour</li>
+					</ul>
+					<p>Until the spell ends, you deal an extra 1d6 damage to a creature you can see whenever you hit it with a weapon attack, and you have advantage on Perception and Survival checks to find it.</p>
+					<!-- BUTTON TRIGGER MODAL -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#huntersMark">
+					  About
+					</button>
+					<!-- END BUTTON TRIGGER MODAL -->
+
+					<!-- MODAL -->
+					<div class="modal fade" id="huntersMark" tabindex="-1" role="dialog" aria-labelledby="huntersMark" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" id="huntersMark">Hunter's Mark Information</h5>
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					      </div>
+					      <div class="modal-body">
+					        <p>If the target drops to 0 hit points before this spell ends, you can use a bonus action on a subsequent turn to mark a new creature. At Higher Levels: When you cast this spell using a spell slot of 3rd or 4th level, you can concentrate for up to 8 hours. If you use a slot of 5th level or higher, you can concentrate for up to 24 hours.</p>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					<!-- END MODAL -->
+				</div>
+				<!-- ------------------------------------------------- -->
+				<!-- SPELL NUMBER FOUR -->
+				<!-- ------------------------------------------------- -->
+				<div class="col-md-4">
+					<h4>Rope Trick</h4>
+					<p>2nd-level transmutation</p>
+					<ul style="list-style: none">
+						<li>Casting Time: 1 action</li>
+						<li>Range: Self </li>
+						<li>Components: V</li>
+						<li>Spell  Duration: 1 hour</li>
+					</ul>
+					<p>You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends. </p>
+					<!-- BUTTON TRIGGER MODAL -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ropeTrick">
+					  About
+					</button>
+					<!-- END BUTTON TRIGGER MODAL -->
+
+					<!-- MODAL -->
+					<div class="modal fade" id="ropeTrick" tabindex="-1" role="dialog" aria-labelledby="ropeTrick" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" id="ropeTrick">Rope Trick</h5>
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					      </div>
+					      <div class="modal-body">
+					        <p>You touch a length of rope that is up to 60 feet long. One end of the rope then rises into the air until the whole rope hangs perpendicular to the ground. At the upper end of the rope, an invisible entrance opens to an extradimensional space that lasts until the spell ends.</p>
+							<p>The extradimensional space can be reached by climbing to the top of the rope. The space can hold as many as eight Medium or smaller creatures. The rope can be pulled into the space, making the rope disappear from view outside the space.</p>
+							<p>Attacks and spells can’t cross through the entrance into or out of the extradimensional space, but those inside can see out of it as if through a 3-foot-by-5-foot window centered on the rope.</p>
+							<p>Anything inside the extradimensional space drops out when the spell ends.</p>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					<!-- END MODAL -->
+				</div>
+			</div> <!-- /COL -->
+		</div>  <!-- COL-XS-12 -->
+	<!-- MAIN CONTENT -->
+</div>
